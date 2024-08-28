87d3739d5e47a75142308a3b0f5025802c11cecf
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2024-08-28 10:32:44 +0530

    feat(cabins): suspense load cabins data

---
+++ src/components/CabinList.tsx
@@ -0,0 +1,16 @@
+import CabinCard from '@/components/CabinCard';
+import { getCabins } from '@/lib/data';
+
+export async function CabinList() {
+  const cabins = await getCabins();
+  if (cabins.length === 0) return null;
+  return (
+    <div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+        {cabins.map((cabin) => (
+          <CabinCard cabin={cabin} key={cabin.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
